74541e5185141bf263fcfdb9e385290ff0cd5585
alfin1945 <alfinmardani7@gmail.com>
2020-09-16 09:30:35 +0700

    Tambahkan file latihan3 programmer pemula

---
+++ Tutoril/pengulangan dan percabangan/if.js
@@ -0,0 +1,12 @@
+var totalBelanja = prompt("total belanja?", 0);
+
+document.write("<h1><B><U>LOMBOK ELEKTRONIK</U></B></h1>");
+
+if (totalBelanja > 150000) {
+  document.write("<h2>Selamat anda mendapatkan Hadiah</h2>");
+}
+
+document.write(
+  "<p><B>Terima Kasih!</B> telah berkunjung/berbelanja di toko kami </p>"
+);
+document.write("Semoga memuaskan anda!");
---
+++ Tutoril/pengulangan dan percabangan/ifelse.js
@@ -0,0 +1,9 @@
+var masukkan = prompt("Masukkan password:");
+
+if (masukkan == "pondok") {
+  document.write("<h2>selamat datang di pondokIT indonesia</h2>");
+} else {
+  document.write("<b>anda salah password</b> ");
+}
+
+document.write("<p>Terima kasih! telah menggunakan aplikasi kami.</p>");
